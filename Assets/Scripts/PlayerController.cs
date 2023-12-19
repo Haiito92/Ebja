60fe9e66f2e26cfe3cddc6eba59486d452c319f1
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2023-12-19 14:24:04 +0100

    Reset

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,32 +3,5 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    //Field
-    float _horizontalMovement;
-    [SerializeField] float speed;
 
-    //Reference
-    Rigidbody2D _rb;
-    private void Awake()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
-
-    public void GetHorizontalMovement(InputAction.CallbackContext ctx)
-    {
-        if(ctx.started) 
-        { 
-            _horizontalMovement = ctx.ReadValue<float>();
-        }
-        else if(ctx.canceled)
-        {
-            _horizontalMovement = 0f;
-        }
-        
-    }
-
-    private void FixedUpdate()
-    {
-        _rb.velocity = new Vector2(_horizontalMovement * speed, _rb.velocity.y);
-    }
 }
