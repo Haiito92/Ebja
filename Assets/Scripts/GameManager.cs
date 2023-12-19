8db39de8271e535aeecd2a453e4699e87ee1c855
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2023-12-19 14:48:00 +0100

    [ADD] Movement Horizontal

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,41 +3,5 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-    //////////////////////
-    /// Test Variables ///
-    //////////////////////
-
-    //Test Movement
-
-
-    //Test Inventory
-    //[SerializeField] Inventory inventory;
-
-    //////////////////////
-    /// Test Functions ///
-    //////////////////////
-    #region TestMovement
-
-    #endregion
-
-    #region Test Inventory
-    //public void SpawnItem(InputAction.CallbackContext ctx)
-    //{
-    //    if(ctx.started)
-    //    {
-    //        ItemData item = new ItemData();
-    //        int index = Random.Range(0, DatabaseManager.Instance.ItemDatabase.Datas.Count);
-    //        item.Init(DatabaseManager.Instance.ItemDatabase.Datas[index]);
-    //        inventory.Add(item);
-    //    }
-    //}
-
-    //public void RemoveItem(InputAction.CallbackContext ctx)
-    //{
-    //    if (ctx.started && inventory.Items.Count > 0)
-    //    {
-    //        inventory.Remove(inventory.Items[0]);
-    //    }
-    //}
-    #endregion
+    
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Fields
+    [SerializeField] float _speed;
 
+    //References
+    Rigidbody2D _rb;
+
+    //InputReferences
+    [SerializeField] InputActionReference _walk;
+
+    //Coroutines
+    Coroutine _walking;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        _walk.action.started += StartWalking;
+        _walk.action.canceled += StopWalking;
+    }
+
+    void StartWalking(InputAction.CallbackContext ctx)
+    {
+        _walking = StartCoroutine(Walking());
+    }
+
+    IEnumerator Walking()
+    {
+        while (true)
+        {
+            Vector2 direction = _walk.action.ReadValue<Vector2>();
+            _rb.velocity = new Vector2(direction.x, _rb.velocity.y)  * _speed * Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    void StopWalking(InputAction.CallbackContext ctx)
+    {
+        if(_walking != null)
+        {
+            StopCoroutine(_walking);
+            _walking = null;
+        }
+        _rb.velocity = Vector2.zero;
+    }
+
+    private void OnDisable()
+    {
+        _walk.action.started -= StartWalking;
+        _walk.action.canceled -= StopWalking;
+    }
 }
