6d6cdb2c275cdf1084ded82524814c1c8c85eea0
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2023-12-19 17:35:31 +0100

    [ADD] Player Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,24 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     //Fields
+    [Header("Fields")]
+    [Space]
     [SerializeField] float _speed;
+
+    [SerializeField] float _jumpForce;
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _groundCheckRadius;
+    bool _isGrounded;
+    [SerializeField] LayerMask _whatIsGround;
 
     //References
     Rigidbody2D _rb;
 
     //InputReferences
+    [Header("InputAction References")]
+    [Space]
     [SerializeField] InputActionReference _walk;
+    [SerializeField] InputActionReference _jump;
 
     //Coroutines
     Coroutine _walking;
@@ -33,10 +44,12 @@ public class PlayerController : MonoBehaviour
 
         _walk.action.started += StartWalking;
         _walk.action.canceled += StopWalking;
+        _jump.action.started += Jump;
 
         OnStartWalkingEvent.AddListener(() => OnStartWalking?.Invoke());
     }
 
+    #region WalkingCoroutine
     void StartWalking(InputAction.CallbackContext ctx)
     {
         if (_walking == null)
@@ -51,7 +64,7 @@ public class PlayerController : MonoBehaviour
         while (true)
         {
             Vector2 direction = _walk.action.ReadValue<Vector2>();
-            _rb.velocity = new Vector2(direction.x, _rb.velocity.y)  * _speed * Time.fixedDeltaTime;
+            _rb.velocity = new Vector2(direction.x * _speed * Time.fixedDeltaTime, _rb.velocity.y);
             yield return new WaitForFixedUpdate();
         }
     }
@@ -63,13 +76,31 @@ public class PlayerController : MonoBehaviour
             StopCoroutine(_walking);
             _walking = null;
         }
-        _rb.velocity = Vector2.zero;
+        _rb.velocity = new Vector2(0, _rb.velocity.y);
         OnStopWalkingEvent?.Invoke();
+    }
+    #endregion
+
+    void Jump(InputAction.CallbackContext ctx)
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _whatIsGround);
+
+        if(_isGrounded)
+        {
+            _rb.AddForce(new Vector2(0, _jumpForce));
+        }
     }
 
     private void OnDisable()
     {
         _walk.action.started -= StartWalking;
         _walk.action.canceled -= StopWalking;
+        _jump.action.started -= Jump;
     }
+
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
+    //}
 }
